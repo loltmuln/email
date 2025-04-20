35155645b66994038fc8fcd3d7e1698926cf1e65
loltmuln <temuulen1211ac@gmail.com>
2025-04-20 19:38:58 +0800

    3

--- forgot-password.php
+++ forgot-password.php
@@ -5,6 +5,7 @@ ini_set('display_errors', 1);
 
 session_start();
 require_once __DIR__ . '/vendor/autoload.php';
+include_once __DIR__ . '/includes/db_config.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
