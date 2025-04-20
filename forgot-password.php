aab1420dbc7d7749c14c89d71516f4f5645175c2
loltmuln <temuulen1211ac@gmail.com>
2025-04-20 19:33:47 +0800

    6

--- forgot-password.php
+++ forgot-password.php
@@ -4,15 +4,10 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 session_start();
-require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/Exception.php';
-require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/PHPMailer.php';
-require_once __DIR__ . '/vendor/phpmailer/phpmailer/src/SMTP.php';
-
-include __DIR__ . '/includes/db_config.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
-use PHPMailer\PHPMailer\SMTP;
 
 $success = '';
 $error = '';
